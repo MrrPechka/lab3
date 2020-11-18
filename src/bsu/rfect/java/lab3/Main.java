c46f6c6238225dd39d654ee29447ee6d35d843ef
Huk Anna <gukanna606@gmail.com>
2020-11-18 17:23:18 +0300

    Fields

--- src/bsu/rfect/java/lab3/Main.java
+++
@@ -1,8 +0,0 @@
-package bsu.rfect.java.lab3;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/bsu/rfect/java/lab3/MainFrame.java
@@ -0,0 +1,45 @@
+package bsu.rfect.java.lab3;
+
+import jdk.nashorn.internal.scripts.JO;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class MainFrame extends JFrame{
+
+    private static final int WIDTH = 1000,
+                             HEIGHT = 500;
+
+    private Double[] coefficients;
+    private JFileChooser fileChooser = null;
+
+    private JMenuItem saveToTextMenuItem,
+                      saveToGraphicsMenuItem,
+                      commaSeparatedValues,
+                      searchValueMenuItem,
+                      searchRangeMenuAction;
+
+    private JMenuItem informationItem;
+
+    private JTextField textFieldFrom,
+                       textFieldTo,
+                       textFieldStep;
+
+    private Box hBoxResult;
+    private DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance();
+    private GornerTableCellRenderer renderer = new GornerTableCellRenderer();
+    private GornerTableModel data;
+
+    public MainFrame(Double[] coefficients){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
