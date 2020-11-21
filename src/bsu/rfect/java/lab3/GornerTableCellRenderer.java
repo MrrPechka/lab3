65aa11f7b94e575dbfdecfe09d9e9dd626051ef0
Huk Anna <gukanna606@gmail.com>
2020-11-21 14:28:44 +0300

    all GornerTableCellRenderer

--- src/bsu/rfect/java/lab3/GornerTableCellRenderer.java
+++ src/bsu/rfect/java/lab3/GornerTableCellRenderer.java
@@ -28,6 +28,45 @@ public class GornerTableCellRenderer implements TableCellRenderer{
     public void setWhichSearch(boolean whichSearch) {this.whichSearch = whichSearch; }
 
     public Component getTableCellRendererComponent(JTable table, Object value,boolean isSelected, boolean hasFocus, int row, int column){
+        String formattedDouble = formatter.format(value);
 
+        if(Double.parseDouble(formattedDouble) < 0.){
+            label.setText(formattedDouble);
+            panel.add(label);
+            panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        }
+
+        if(Double.parseDouble(formattedDouble) == 0.){
+            label.setText(formattedDouble);
+            panel.add(label);
+            panel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        }
+
+        if(Double.parseDouble(formattedDouble) > 0.){
+            label.setText(formattedDouble);
+            panel.add(label);
+            panel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        }
+
+        if(!this.whichSearch){
+            if(needle != null && needle.equals(formattedDouble))
+                panel.setBackground(Color.RED);
+            else
+                panel.setBackground(Color.WHITE);
+        }
+
+        if(this.whichSearch){
+            if(needle != null){
+                Double dPlus = Double.parseDouble(needle) + 0.1;
+                Double dMinus = Double.parseDouble(needle) - 0.1;
+                String plus = formatter.format(dPlus);
+                String minus = formatter.format(dMinus);
+                if(plus.equals(formattedDouble) || minus.equals(formattedDouble))
+                    panel.setBackground(Color.RED);
+                else
+                    panel.setBackground(Color.WHITE);
+            }
+        }
+        return panel;
     }
 }
