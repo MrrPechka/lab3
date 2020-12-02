c6a7db97449fd1a626c28fb7a13fa7978921abf6
Huk Anna <gukanna606@gmail.com>
2020-12-02 16:24:34 +0300

    fix GornerTableCellRenderer

--- src/bsu/rfect/java/lab3/GornerTableCellRenderer.java
+++ src/bsu/rfect/java/lab3/GornerTableCellRenderer.java
@@ -7,14 +7,14 @@ import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
 
-public class GornerTableCellRenderer implements TableCellRenderer{
-    private JPanel panel = new JPanel();
-    private JLabel label = new JLabel();
+public class GornerTableCellRenderer implements TableCellRenderer {
     private String needle = null;
+    private JLabel label = new JLabel();
+    private JPanel panel = new JPanel();
     private DecimalFormat formatter = (DecimalFormat)NumberFormat.getInstance();
     private boolean whichSearch = false;
 
-    public GornerTableCellRenderer() {
+    public GornerTableCellRenderer(){
         panel.add(label);
         panel.setLayout(new FlowLayout(FlowLayout.LEFT));
         formatter.setMaximumFractionDigits(5);
@@ -24,44 +24,48 @@ public class GornerTableCellRenderer implements TableCellRenderer{
         formatter.setDecimalFormatSymbols(dottedDouble);
     }
 
-    public void setNeedle(String needle){ this.whichSearch = whichSearch; }
-    public void setWhichSearch(boolean whichSearch) {this.whichSearch = whichSearch; }
+    public void setNeedle(String needle) {
+        this.needle = needle;
+    }
+    public void setWhichSearch(boolean whichSearch) {
+        this.whichSearch = whichSearch;
+    }
 
-    public Component getTableCellRendererComponent(JTable table, Object value,boolean isSelected, boolean hasFocus, int row, int column){
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value,
+                                                   boolean isSelected, boolean hasFocus,
+                                                   int row, int column) {
         String formattedDouble = formatter.format(value);
-
-        if(Double.parseDouble(formattedDouble) < 0.){
+        if (Double.parseDouble(formattedDouble) < 0.){
             label.setText(formattedDouble);
             panel.add(label);
             panel.setLayout(new FlowLayout(FlowLayout.LEFT));
         }
-
-        if(Double.parseDouble(formattedDouble) == 0.){
+        if (Double.parseDouble(formattedDouble) == 0.){
             label.setText(formattedDouble);
             panel.add(label);
             panel.setLayout(new FlowLayout(FlowLayout.CENTER));
         }
-
-        if(Double.parseDouble(formattedDouble) > 0.){
+        if (Double.parseDouble(formattedDouble) > 0.){
             label.setText(formattedDouble);
             panel.add(label);
             panel.setLayout(new FlowLayout(FlowLayout.RIGHT));
         }
 
-        if(!this.whichSearch){
-            if(needle != null && needle.equals(formattedDouble))
-                panel.setBackground(Color.RED);
+        if (!this.whichSearch) {
+            if (needle != null && needle.equals(formattedDouble))
+                panel.setBackground(Color.ORANGE);
             else
                 panel.setBackground(Color.WHITE);
         }
 
-        if(this.whichSearch){
-            if(needle != null){
+        if (this.whichSearch){
+            if (needle!= null){
                 Double dPlus = Double.parseDouble(needle) + 0.1;
                 Double dMinus = Double.parseDouble(needle) - 0.1;
                 String plus = formatter.format(dPlus);
                 String minus = formatter.format(dMinus);
-                if(plus.equals(formattedDouble) || minus.equals(formattedDouble))
+                if (plus.equals(formattedDouble) || minus.equals(formattedDouble))
                     panel.setBackground(Color.RED);
                 else
                     panel.setBackground(Color.WHITE);
